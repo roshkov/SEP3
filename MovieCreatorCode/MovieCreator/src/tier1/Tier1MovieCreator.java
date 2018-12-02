d00669f296dc736d644a41c6a37d803b4f931048
Stefan Harabagiu <stefan.harabagiu@gmail.com>
2018-12-03 00:53:47 +0100

    Functionality to buttons, T1 main uses GUI

--- MovieCreatorCode/MovieCreator/src/tier1/Tier1MovieCreator.java
+++ MovieCreatorCode/MovieCreator/src/tier1/Tier1MovieCreator.java
@@ -1,7 +1,7 @@
 package tier1;
 
 import tier1.controller.Tier1MovieCreatorController;
-import tier1.view.Tier1MovieCreatorConsole;
+import tier1.view.Tier1MovieCreatorGUI;
 import tier1.view.Tier1MovieCreatorView;
 
 
@@ -13,7 +13,7 @@ public class Tier1MovieCreator {
 	
 	public static void main(String[] args) 
 	{	//view
-		Tier1MovieCreatorView view = new Tier1MovieCreatorConsole();					
+		Tier1MovieCreatorView view = new Tier1MovieCreatorGUI();					
 		//controller
 		Tier1MovieCreatorController controller = new Tier1MovieCreatorController(view); 
 		//starts thread in it
--- MovieCreatorCode/MovieCreator/src/tier1/view/Tier1MovieCreatorGUI.java
+++ MovieCreatorCode/MovieCreator/src/tier1/view/Tier1MovieCreatorGUI.java
@@ -14,6 +14,7 @@ import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
@@ -23,35 +24,23 @@ import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import javax.swing.SwingConstants;
 
-public class Tier1MovieCreatorGUI {
+import tier1.controller.Tier1MovieCreatorController;
 
+public class Tier1MovieCreatorGUI implements Tier1MovieCreatorView, Runnable {
+
+	private Tier1MovieCreatorController controller;
+	
 	private JFrame frame;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Tier1MovieCreatorGUI window = new Tier1MovieCreatorGUI();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
 	/**
 	 * Create the application.
 	 */
 	public Tier1MovieCreatorGUI() {
-		initialize();
 	}
 
 	/**
 	 * Initialize the contents of the frame.
+	 * @wbp.parser.entryPoint
 	 */
 	private void initialize() {
 		frame = new JFrame();
@@ -60,18 +49,78 @@ public class Tier1MovieCreatorGUI {
 		frame.getContentPane().setLayout(null);
 		
 		JButton createMovie = new JButton("Create Movie");
+		createMovie.setBackground(new Color(255, 145, 164));
+		createMovie.setContentAreaFilled(false);
+        createMovie.setOpaque(true);
 		createMovie.setBounds(37, 108, 126, 73);
+		createMovie.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                  createMovieActionPerformed(evt);
+            }
+        });
 		frame.getContentPane().add(createMovie);
 		
-		JButton btnNewButton_1 = new JButton("List Movies");
-		btnNewButton_1.setBounds(229, 108, 126, 73);
-		frame.getContentPane().add(btnNewButton_1);
+		JButton listMovies = new JButton("List Movies");
+		listMovies.setBackground(new Color(255, 145, 164));
+		listMovies.setContentAreaFilled(false);
+		listMovies.setOpaque(true);
+		listMovies.setBounds(229, 108, 126, 73);
+		frame.getContentPane().add(listMovies);
+		listMovies.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                listMoviesActionPerformed(evt);
+          }
+      });
 		
-		JButton btnNewButton_2 = new JButton("Quit");
-		btnNewButton_2.setBounds(425, 108, 126, 73);
-		frame.getContentPane().add(btnNewButton_2);
+		JButton quitButton = new JButton("Quit");
+		quitButton.setBackground(new Color(255, 145, 164));
+		quitButton.setContentAreaFilled(false);
+		quitButton.setOpaque(true);
+		quitButton.setBounds(425, 108, 126, 73);
+		frame.getContentPane().add(quitButton);
+		quitButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                quitButtonActionPerformed(evt);
+          }
+      });
+		
 		frame.setBackground(new Color(0, 0, 0));
 		frame.setBounds(100, 100, 600, 357);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	private void createMovieActionPerformed(java.awt.event.ActionEvent evt) {
+		controller.execute(1);	
+	}
+	
+	private void listMoviesActionPerformed(java.awt.event.ActionEvent evt) {
+		controller.execute(2);	
+	}
+	
+	private void quitButtonActionPerformed(java.awt.event.ActionEvent evt) {
+		controller.execute(0);	
+	}
+	
+	@Override
+	public String get(String string) {
+		return (String)JOptionPane.showInputDialog(string);
+	}
+
+	@Override
+	public void show(String text) {
+		JOptionPane.showMessageDialog(null, text);	
+	}
+
+	@Override
+	public void startView(Tier1MovieCreatorController controller) {
+		this.controller = controller;
+		Thread t = new Thread(this);
+		t.start();
+	}
+
+	@Override
+	public void run() {
+		initialize();
+		this.frame.setVisible(true);
 	}
 }
