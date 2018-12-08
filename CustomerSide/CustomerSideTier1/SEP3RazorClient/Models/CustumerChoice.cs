21545e54dc63f72f849150608453a63f61c974e2
vidwalk <claudiurediu@gmail.com>
2018-12-08 16:55:10 +0100

    CustomerSideTier1 Waiting for WebApi

---
+++ CustomerSide/CustomerSideTier1/SEP3RazorClient/Models/CustumerChoice.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEP3RazorClient.Models
+{
+    public class CustumerChoice
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ CustomerSide/CustomerSideTier1/SEP3RazorClient/Pages/BookSeats.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using SEP3RazorClient.Models;
+
+namespace SEP3RazorClient.Pages
+{
+    public class SeatsModel : PageModel
+    {
+        private Shedule shedule;
+
+        public ScheduledMovie sm { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public int Choice { get; set; }
+        public Shedule Shedule { get => shedule; set => shedule = value; }
+        [BindProperty]
+        public int SeatChoice { get; set; }
+
+        [HttpGet]
+        public void OnGet(int choice)
+        {
+           
+            // get from api
+            Shedule = new Shedule();
+           
+        }
+
+        public ActionResult OnPost()
+        {  
+            System.Console.WriteLine((Int32.Parse(Request.Form["SeatChoice"]) - 1));
+            return Redirect("/BookSeats/" + Choice);
+        }
+    }
+}
