59d70b1827da11b6959818af19b6db9f48c2abdb
vidwalk <claudiurediu@gmail.com>
2018-12-14 22:00:15 +0100

    added API documentation, can be checked at https://localhost:5003/swagger/index.html after starting it

--- CustomerSide/Tier2WebApi/Startup.cs
+++ CustomerSide/Tier2WebApi/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 using Tier2WebApi.Communication;
 
 namespace Tier2WebApi
@@ -28,6 +29,11 @@ namespace Tier2WebApi
         {
             services.AddSingleton<Tier2JavaProvider>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+              services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "API Tier2", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +47,17 @@ namespace Tier2WebApi
             {
                 app.UseHsts();
             }
+
+                // Enable middleware to serve generated Swagger as a JSON endpoint.
+                app.UseSwagger();
+
+                // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+                // specifying the Swagger JSON endpoint.
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                });
+
 
             app.UseHttpsRedirection();
             app.UseMvc();
